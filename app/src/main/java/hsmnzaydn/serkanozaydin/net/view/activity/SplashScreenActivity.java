f17b4bb98f2c119c1305f2bd50602e40af42893d
hsmnzaydn <serkan1994>
2017-12-19 04:45:30 +0300

    Added splash Screen

--- app/src/main/java/hsmnzaydn/serkanozaydin/net/view/activity/SplashScreenActivity.java
+++ app/src/main/java/hsmnzaydn/serkanozaydin/net/view/activity/SplashScreenActivity.java
@@ -1,10 +1,10 @@
 package hsmnzaydn.serkanozaydin.net.view.activity;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import hsmnzaydn.serkanozaydin.net.R;
 import hsmnzaydn.serkanozaydin.net.view.activity.category.CategoryListActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
@@ -14,8 +14,14 @@ public class SplashScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        Intent intent=new Intent(SplashScreenActivity.this, CategoryListActivity.class);
-        startActivity(intent);
-        this.finish();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashScreenActivity.this, CategoryListActivity.class);
+                startActivity(intent);
+                SplashScreenActivity.this.finish();
+            }
+        },3000);
+
     }
 }
